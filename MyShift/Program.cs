14a438d6b64e1ab4872f38467d0b05002ff95168
Dmitry <vatuev.dima@yandex.ru>
2026-01-22 13:42:25 +0400

    Протестирована работа бота в первый раз

--- MyShift/Program.cs
+++ MyShift/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace MyShift
+﻿using Telegram.Bot;
+
+namespace MyShift
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static string _token = "";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать в бот планировщик. Ниже описаны возможные команды.");
+            ITelegramBotClient botClient = new TelegramBotClient(_token);
+            //var me = await botClient.GetMe();
+            var Handle = new UpdateHandler();
+            botClient.StartReceiving(Handle);
+            await Task.Delay(-1);
         }
     }
 }
---
+++ MyShift/UpdateHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace MyShift
+{
+    internal class UpdateHandler : IUpdateHandler
+    {
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            botClient.SendMessage(update.Message.Chat, $"Повторяю:{update.Message.Text}");
+            return Task.CompletedTask;
+        }
+    }
+}
