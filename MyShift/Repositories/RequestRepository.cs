cf31bc990b3b310a08740ef8b1520d863d519e9a
Dmitry <vatuev.dima@yandex.ru>
2026-01-24 12:31:41 +0400

    Добавлен сервис для пользования заявками и рассписанием.

---
+++ MyShift/Repositories/RequestRepository.cs
@@ -0,0 +1,32 @@
+﻿using MyShift.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShift.Repositories
+{
+    internal class RequestRepository : IRequestRepository
+    {
+        private readonly SqLiteDbContext _context;
+        public RequestRepository(SqLiteDbContext context)
+        {
+            _context = context;
+        }
+        public void ApproveRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RejectRequest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyShift/Repositories/ScheduleRepository.cs
@@ -0,0 +1,32 @@
+﻿using MyShift.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShift.Repositories
+{
+    internal class ScheduleRepository : IScheduleRepository
+    {
+        private readonly SqLiteDbContext _context;
+        public ScheduleRepository(SqLiteDbContext context)
+        {
+            _context = context;
+        }
+        public void CreateSchedule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSchedule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditShiftSchedule()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
