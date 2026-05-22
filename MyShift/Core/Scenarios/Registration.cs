cf4e4eb7a1ce0cfee2047e55531b5d970d9b12ec
Dmitry <vatuev.dima@yandex.ru>
2026-05-22 19:25:01 +0400

    Фикс смены кнопок при подтверждении регистрации

--- MyShift/Core/Scenarios/Registration.cs
+++ MyShift/Core/Scenarios/Registration.cs
@@ -55,9 +55,10 @@ namespace MyShift.Core.Scenarios
                     Validator.ValidateCurrentMessage((Message)context.Data["currentMessage"], message, 0);
                     Role role = (Role)Int32.Parse(context.Data["Callback"].ToString());
                     int userRegId = Int32.Parse(context.Data["userId"].ToString());
+                    ToDoUser currentUser = await _userService.GetUserByTelegramIdAsync(message.Chat.Id, ct);
                     ToDoUser userRegistration = await _userService.GetUserAsync(userRegId, ct);
                     await _userService.SetRoleAsync(userRegistration.Id, role, ct);
-                    await botClient.SendMessage(message.Chat, $"Пользователелю выдана роль \"{role.GetDisplayName()}\" успешно добавлен.", cancellationToken: ct);
+                    await botClient.SendMessage(message.Chat, $"Пользователелю выдана роль \"{role.GetDisplayName()}\" успешно добавлен.", replyMarkup: MarkupManager.SetStandartKeyboardButtonList(currentUser.Role), cancellationToken: ct);
                     await botClient.SendMessage(userRegistration.TelegramId, $"Ваша регистрация завершена. Вам выдана роль \"{role.GetDisplayName()}\"", replyMarkup:MarkupManager.SetStandartKeyboardButtonList(role), cancellationToken: ct);
                     MarkupManager.SetCommand(botClient, role, userRegistration.TelegramId, ct);
                     break;
