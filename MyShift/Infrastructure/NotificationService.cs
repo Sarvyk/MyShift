c1b1ab94f435e7ac2a32020051028194b294578d
Dmitry <vatuev.dima@yandex.ru>
2026-05-22 20:38:38 +0400

    дофикс фикса структуры

--- MyShift/Infrastructure/NotificationService.cs
+++ MyShift/Infrastructure/NotificationService.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 
-namespace MyShift
+namespace MyShift.Infrastructure
 {
     internal class NotificationService : INotificationService
     {
